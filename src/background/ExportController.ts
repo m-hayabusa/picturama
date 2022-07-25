b812af797fc287c31ca0200cfd89019fb9910546
hayabusa <s.hashimoto@hs-sh.net>
2022-07-25 19:18:15 +0900

    ExportをSharpでやるようにした

--- src/background/ExportController.ts
+++ src/background/ExportController.ts
@@ -1,4 +1,5 @@
-import piexif from 'piexifjs'
+// import piexif from 'piexifjs'
+import Sharp from "sharp"
 
 import { getMasterPath } from 'common/util/DataUtil'
 import { Size } from 'common/util/GeometryTypes'
@@ -6,15 +7,14 @@ import { parsePath } from 'common/util/TextUtil'
 import { Photo, PhotoExportOptions } from 'common/CommonTypes'
 
 import { fetchPhotoWorkOfPhoto } from 'background/store/PhotoWorkStore'
-import { fsExists, fsWriteFile, fsReadFile, fsStat, fsUtimes } from 'background/util/FileUtil'
-import ForegroundClient from 'background/ForegroundClient'
+import { fsExists, fsStat, fsUtimes } from 'background/util/FileUtil'
 
 
 const jpgExtensionRE = new RegExp(`\\.jpe?g$`, 'i')
 
 
-export async function exportPhoto(photo: Photo, photoIndex: number, options: PhotoExportOptions): Promise<void> {
-    const masterPath = getMasterPath(photo)
+export async function exportPhoto(photo: Photo, photoIndex: number, options: PhotoExportOptions, overwrite = false): Promise<void> {
+    const masterPath = getMasterPath(photo);
 
     let maxSize: Size | null
     switch (options.size) {
@@ -50,7 +50,6 @@ export async function exportPhoto(photo: Photo, photoIndex: number, options: Pho
     }
 
     const photoWork = await fetchPhotoWorkOfPhoto(photo)
-    let imageBinaryString = await ForegroundClient.renderPhoto(photo, photoWork, maxSize, options)
 
     let exportFileBasePath: string
     switch (options.fileNameStyle) {
@@ -72,16 +71,59 @@ export async function exportPhoto(photo: Photo, photoIndex: number, options: Pho
         const suffix = counter === 0 ? '' : ('_' + (counter < 10 ? '00' : counter < 100 ? '0' : '') + counter)
         exportFilePath = `${exportFileBasePath}${suffix}.${options.format}`
         counter++
-    } while (await fsExists(exportFilePath))
+    } while (!overwrite && await fsExists(exportFilePath))
 
-    if (options.withMetadata && options.format === 'jpg' && jpgExtensionRE.test(masterPath)) {
-        const masterImageBuffer = await fsReadFile(masterPath)
-        const masterImageData = masterImageBuffer.toString('binary')
-        const metaData = piexif.load(masterImageData)
-        imageBinaryString = piexif.insert(piexif.dump(metaData), imageBinaryString)
+    Sharp.cache(false)
+    let image = Sharp(masterPath)
+
+    if (photoWork.rotationTurns)
+        image = image.rotate(photoWork.rotationTurns * 90)
+
+    if (photoWork.cropRect) {
+        let size: Sharp.Region = {
+            left: 0,
+            top: 0,
+            width: 0,
+            height: 0
+        };
+
+        let width:number, height:number
+
+        if (photoWork.rotationTurns === 1 || photoWork.rotationTurns === 3) {
+            width = photo.master_height
+            height = photo.master_width
+        } else {
+            width = photo.master_width
+            height = photo.master_height
+        }
+
+        size.left = width / 2 + photoWork.cropRect.x
+        size.top = height / 2 + photoWork.cropRect.y
+        size.width = photoWork.cropRect.width
+        size.height = photoWork.cropRect.height
+
+        size.left = Math.round(size.left)
+        size.top = Math.round(size.top)
+        size.width = Math.round(size.width)
+        size.height = Math.round(size.height)
+
+        image = image.extract(size)
     }
 
-    await fsWriteFile(exportFilePath, imageBinaryString, 'binary')
+    if (maxSize) {
+        image = image.resize(maxSize.width, maxSize.height)
+    }
+
+    await image.toFormat(options.format).toFile(exportFilePath)
+
+    // if (options.withMetadata && options.format === 'jpg' && jpgExtensionRE.test(masterPath)) {
+    //     const masterImageBuffer = await fsReadFile(masterPath)
+    //     const masterImageData = masterImageBuffer.toString('binary')
+    //     const metaData = piexif.load(masterImageData)
+
+    //     await exiftool.write (exportFilePath, await exiftool.read(masterPath))
+    //     imageBinaryString = piexif.insert(piexif.dump(metaData), imageBinaryString)
+    // }
 
     const masterStat = await fsStat(masterPath)
     await fsUtimes(exportFilePath, masterStat.atime, masterStat.mtime)
